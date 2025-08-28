ee5663ef3440e56e5c8ffb1f809ceeb735698b37
RamildoJunior <rpsjuninho@gmail.com>
2025-08-27 21:12:41 -0300

    iniciando tests

---
+++ src/tests/entities/FinancingTests.java
@@ -0,0 +1,16 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Test;
+
+public class FinancingTests {
+
+    @Test
+    public void CreateObjctWithCorrectDataWhenIsValid(){
+
+
+    }
+
+
+
+
+}
