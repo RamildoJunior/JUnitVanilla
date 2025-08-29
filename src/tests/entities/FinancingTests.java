aee873d689feea73592a361eaba4f8771976f250
RamildoJunior <rpsjuninho@gmail.com>
2025-08-28 21:32:06 -0300

    Concluindo primeira etapa dos tests

--- src/tests/entities/FinancingTests.java
+++ src/tests/entities/FinancingTests.java
@@ -1,14 +1,77 @@
 package tests.entities;
 
+import entities.Financing;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FinancingTests {
 
     @Test
-    public void CreateObjctWithCorrectDataWhenIsValid(){
+    public void constructorShouldCreateObjectWhenValidData(){
+
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        Assertions.assertEquals(100000.0, f.getTotalAmount());
+        Assertions.assertEquals(2000.0, f.getIncome());
+        Assertions.assertEquals(80, f.getMonths());
+
+    }
+
+    @Test
+    public void constructorShouldThrowIllegalArgumentExceptionWhenValidData(){
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 20);
+
+        });
+
+    }
+
+    @Test
+    public void setTotalAmountShouldSetDataWhenValidData(){
+
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        f.setTotalAmount(90000.0);
+
+        Assertions.assertEquals(90000.0, f.getTotalAmount());
 
 
     }
+
+    @Test
+    public void setTotalAmountShouldThrowIllegalArgumentExceptionWhenValidData() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 80);
+            f.setTotalAmount(150000.0);
+
+        });
+    }
+
+        @Test
+        public void setIncomeShouldSetDataWhenValidData(){
+
+            Financing f = new Financing(100000.0, 2000.0, 80);
+
+            f.setIncome(2100.0);
+
+            Assertions.assertEquals(2100.0, f.getIncome());
+
+
+        }
+
+        @Test
+        public void setIncomeShouldThrowIllegalArgumentExceptionWhenValidData(){
+
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Financing f = new Financing(100000.0, 2000.0, 80);
+                f.setIncome(1900.0);
+
+            });
+    }
+
+
 
 
 
